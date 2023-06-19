d41a26e1bbfef1bbf51c1858d2a601d0172a9799
Yuri Castilho <yuricastilho2005@gmail.com>
2023-06-18 21:26:21 -0300

    adicionado components: about - contact - projects

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,13 +4,13 @@ const Hero = () => {
   return (
     <div className="text-[#141C3A]">
       <div className="mx-auto mt-[-200px] flex h-screen w-full max-w-[800px] flex-col justify-center text-center">
-        <h1 className="text-3xl font-bold sm:text-4xl md:text-5xl">
+        <h1 className="text-3xl font-bold md:text-5xl">
           Designer & Frontend Developer
         </h1>
-        <p className="max-sm mt-5 text-xs sm:text-sm md:text-lg">
-          Dedicado a desenvolver interfaces incríveis e intuitivas que fazem a
-          diferença.
-        </p>
+        <h3 className="mt-5 text-xl">
+          Dedicado a desenvolver interfaces incríveis e intuitivas <br /> que
+          fazem a diferença.
+        </h3>
       </div>
     </div>
   )

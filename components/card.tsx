246013e6cef2b96c5e5802c7dfe80006057bb6d9
Yuri Castilho <yuricastilho2005@gmail.com>
2023-06-28 22:49:10 -0300

    text card design

--- components/card.tsx
+++ components/card.tsx
@@ -5,7 +5,10 @@ const Card = () => {
     <div className="mx-auto flex h-[760px] w-[900px] justify-between rounded-2xl bg-white">
       <div className="w-1/2 items-center justify-center border-r-2 text-center">
         <h1 className="mt-16 text-3xl font-bold text-black">Design</h1>
-        <p className="mt-6 text-black">djasbabsidkbasdasa</p>
+        <p className="mt-6 text-black">
+          Eu valorizo designs limpos e intuitivos, sempre utilizando de bons
+          costumes de UI/UX
+        </p>
       </div>
       <div className="w-1/2 items-center justify-center text-center ">
         <h1 className="mt-16 text-3xl font-bold text-black">Frontend</h1>
